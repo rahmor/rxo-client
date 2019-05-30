e874c0a1b44c7424401113450ab37af8d69d8e37
rahmor <rahmor@yahoo.com>
2019-05-30 09:33:47 -0400

    add api call to register componenent

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Heading from '../Heading/Heading';
 import ApiService from '../services/api-services';
 import './Register.css';
@@ -7,17 +8,17 @@ class Register extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      error: null
+      error: null,
+      login: null
     };
   }
   handleRegistrationSubmit = event => {
+    //get notice your registration was successful and link to login page.
     event.preventDefault();
     const { username, password } = event.target;
     ApiService.postRegistration(username.value, password.value)
       .then(response => {
-        username.value = '';
-        password.value = '';
-        this.props.history.replace('/login');
+        this.setState({ login: response.username });
       })
       .catch(response => this.setState({ error: response.error }));
   };
@@ -35,6 +36,15 @@ class Register extends Component {
             <br />
             <br />
             <input type='password' name='password' placeholder='Password' />
+            <div role='alert'>
+              {(this.state.error && <p>{this.state.error}</p>) ||
+                (this.state.login && (
+                  <p>
+                    You're registration was {this.state.login}.{' '}
+                    <Link to='/login'>Log In</Link>{' '}
+                  </p>
+                ))}
+            </div>
             <br />
             <br />
             <input type='submit' value='Register' />
